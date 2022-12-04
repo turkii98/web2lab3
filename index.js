670fa3739b24490cbcb8decf19177f9c60e0b6c2
Matija Turkalj <matija.turkalj@fer.hr>
2022-12-04 01:18:53 +0100

    render

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const externalUrl = process.env.RENDER_EXTERNAL_URL;
+const port= externalUrl && process.env.PORT ? parseInt(process.env.PORT) : 3000
 
 app.get('/', (req, res) => {
   res.render('index.ejs')
